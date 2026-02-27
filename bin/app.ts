5b7228dfa368e7b282ea2e1b15f1289fc89ff279
Rahul <apple@apples-MacBook-Pro.local>
2026-02-27 23:14:09 +0530

    Updated the readme.md

--- bin/app.ts
+++ bin/app.ts
@@ -1,13 +1,23 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ApiStack } from '../lib/api-stack';
+import { MessagingStack } from '../lib/messaging-stack';
 import { WorkerStack } from '../lib/worker-stack';
 
 const app = new cdk.App();
 
-const apiStack = new ApiStack(app, 'ApiStack');
+const messagingStack = new MessagingStack(app, 'MessagingStack');
 
-// Pass tables into worker stack via props
+const apiStack = new ApiStack(app, 'ApiStack', {
+  orderTopic: messagingStack.orderTopic,
+  ordersTable: messagingStack.ordersTable,
+  inventoryTable: messagingStack.inventoryTable,
+});
+
 new WorkerStack(app, 'WorkerStack', {
-  ordersTable: apiStack.ordersTable,
-  inventoryTable: apiStack.inventoryTable,
+  inventoryQueue: messagingStack.inventoryQueue,
+  notificationQueue: messagingStack.notificationQueue,
+  ordersTable: messagingStack.ordersTable,
+  inventoryTable: messagingStack.inventoryTable,
 });
